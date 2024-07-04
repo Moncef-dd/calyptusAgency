893beee234e4ae0c66561b5cbff4f016bcad22b9
Moncef SAMAI <Moncef.samai.rol2@gmail.com>
2024-07-03 20:41:34 -0400

    Navbar

---
+++ web/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-list">
+        <li className="navbar-item">Home</li>
+        <li className="navbar-item">About</li>
+        <li className="navbar-item">Services</li>
+        <li className="navbar-item">Contact</li>
+      </ul>
+      <div className="navbar-logo">
+        <img src="/path/to/logo.png" alt="Logo" />
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
